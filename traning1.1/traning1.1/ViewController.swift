337522a6cc72ad2487d85128afb41bc16ee3607f
NgocHap <ngochap98@gmail.com>
2022-03-15 09:13:36 +0700

    orign

---
+++ traning1.1/traning1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traning1.1
+//
+//  Created by NgocHap on 15/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
